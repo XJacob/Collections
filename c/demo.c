003e61643d7d4aee54ad03e7da79696ca7180b49
jacob.lee <jacob.lee@deltaww.com>
2016-09-08 13:09:23 +0900

    [C] Add function to read config file
    read config file and pasring the key/value.

--- c/demo.c
+++ c/demo.c
@@ -10,9 +10,10 @@
 
 #include "demo.h"
 
-static char* const short_options = "l:h";
+static char* const short_options = "l:hc:";
 static struct option long_options[] = {
      { "log",	1,	NULL,	'l'},
+     { "config",	1,	NULL,	'c'},
      { "help",	0,	NULL,	'h'},
      {  NULL,	0,	NULL,	0},
 };
@@ -39,17 +40,88 @@ static void usage(const char *name)
 {
 	printf("usage::\n");
 	printf("%s [OPTIONS]...\n", name);
+	printf("  -c [--config]  config file\n");
 	printf("  -l [--log]   saving to log file\n");
 	printf("  -h [--help]  help file\n");
 	printf("\n\n\n");
 }
 
+// direction : 1-> left, 2->right, 3->both side
+static void remove_extra_space(char *val, int direction)
+{
+	int idx = 0;
+	int len;
+
+	if (direction == 1 || direction == 3) {
+		 len = strlen(val) + 1;
+		 while(val[idx] != '\0' && val[idx] == ' ')
+			  idx++;
+		 if (val[idx] == '\0')
+			  return;
+		 memmove(val, val + idx, len - idx);
+	}
+	if (direction == 2 || direction == 3) {
+		 len = strlen(val) - 1;
+		 idx = len;
+
+		 while(idx && (val[idx] == ' ' ||  val[idx] == '\n'))
+			  idx--;
+		 val[idx + 1] = '\0';
+	}
+}
+
+static void process_config(const char *cfg_name, bool show_cfg)
+{
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0, read;
+	char *default_name = "demo.cfg";
+	char *value, *name;
+	char *sperator;
+
+	if (!cfg_name)
+		 cfg_name = default_name;
+
+	fp = fopen(cfg_name, "r");
+
+	if (!fp) {
+		fprintf(stderr, "error when opening file %s : ", cfg_name != NULL ? cfg_name : "demo.cfg");
+		perror("");
+		return;
+	}
+
+	value = NULL;
+	name = NULL;
+	print(" Config file: %s\n", cfg_name);
+	while ((read = getline(&line, &len, fp)) != -1) {
+	     if (line[0] != '#' && line[0] != ' ') {
+		  sperator = strchr((const char *)line, (int)'=');
+		  if (sperator) {
+		       name = realloc(name, sperator - line);
+		       value = realloc(value, strlen(sperator+1));
+
+		       strncpy(value, sperator+1, strlen(sperator+1));
+		       strncpy(name, line, sperator-line);
+		       remove_extra_space(name, 2);
+		       remove_extra_space(value, 3);
+		       if (show_cfg)
+			    print("     name= %s value= %s\n", name, value);
+		  }
+	     }
+	}
+	CHK_AND_FREE(line);
+	CHK_AND_FREE(name);
+	CHK_AND_FREE(value);
+	fclose(fp);
+}
+
 int main(int argc, char *argv[])
 {
 	int opt;
-	char *log_name;
+	char *log_name, *cfg_name;
 
 	log_name = NULL;
+	cfg_name = NULL;
 	while((opt = getopt_long(argc, argv, short_options, long_options, NULL)) != -1 ) {
 		switch(opt) {
 			case 'l':
@@ -57,6 +129,12 @@ int main(int argc, char *argv[])
 				if (!log_name)
 					perror("out of memory");
 				strncpy(log_name, optarg, strlen(optarg));
+				break;
+			case 'c':
+				cfg_name = malloc(strlen(optarg));
+				if (!cfg_name)
+					perror("out of memory");
+				strncpy(cfg_name, optarg, strlen(optarg));
 				break;
 			case 'h':
 				usage(argv[0]);
@@ -72,6 +150,8 @@ int main(int argc, char *argv[])
 		}
 		print("open log file: %s, %s\n", log_name, log_file ? "success" : "failed");
 	}
+
+	process_config(cfg_name, true);
 
 	if (log_name)
 		free(log_name);
--- c/demo.h
+++ c/demo.h
@@ -1,6 +1,9 @@
 #ifndef __DEMO_H__
 #define __DEMO_H__
 
+#define CHK_AND_FREE(x) do { \
+							if(x) free(x);	\
+						} while(0);	\
 
 extern void print(const char *, ...);
 
